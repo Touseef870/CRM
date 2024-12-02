8c330c34977f4f54d85e4d43173fd07c3fd107d3
Fakhir-Iqbal <fakhiriqbal>
2024-12-02 21:29:32 +0500

    Set Loader....

--- src/components/dashboard/overview/latest-employ.tsx
+++ src/components/dashboard/overview/latest-employ.tsx
@@ -7,6 +7,7 @@ import { ArrowRight as ArrowRightIcon } from '@phosphor-icons/react';
 import dayjs from 'dayjs';
 import Link from 'next/link';
 import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export interface Employ {
   id: string;
@@ -25,6 +26,7 @@ export interface LatestEmployProps {
 export const LatestEmploy: React.FC<LatestEmployProps> = ({ employ = [], sx }) => {
   const [userType, setUserType] = useState<string>('');
   const [employData, setEmployData] = useState<Employ[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
 
   useEffect(() => {
@@ -35,6 +37,7 @@ export const LatestEmploy: React.FC<LatestEmployProps> = ({ employ = [], sx }) =
           const parsedData = JSON.parse(adminData);
           setUserType(parsedData.type || '');
         } catch (error) {
+          setLoading(false)
           console.error('Error parsing AdminloginData:', error);
         }
       }
@@ -51,8 +54,11 @@ export const LatestEmploy: React.FC<LatestEmployProps> = ({ employ = [], sx }) =
               Authorization: `Bearer ${token}`,
             },
           });
-          setEmployData(response.data.data.employees.slice(0, 4));
+          setEmployData(response.data.data.employees.slice(0, 5));
+          setLoading(false)
+
         } catch (error) {
+          setLoading(false)
           console.log('Error fetching employee data:', error);
         }
       }
@@ -65,39 +71,46 @@ export const LatestEmploy: React.FC<LatestEmployProps> = ({ employ = [], sx }) =
   if (userType === 'employee') return null;
 
   return (
-    <Card sx={sx}>
+    <Card sx={sx} style={{ height: "auto" }}>
       <CardHeader title="Recent Join Employ" />
       <Divider />
-      <List>
-        {employData.map((employee, index) => (
-          <ListItem divider={index < employData.length - 1} key={index}>
-            <ListItemAvatar>
-              {employee.image ? (
-                <Box
-                  component="img"
-                  src={employee.image}
-                  sx={{ borderRadius: 1, height: '48px', width: '48px' }}
-                />
-              ) : (
-                <Box
-                  sx={{
-                    borderRadius: 1,
-                    backgroundColor: 'var(--mui-palette-neutral-200)',
-                    height: '48px',
-                    width: '48px',
-                  }}
-                />
-              )}
-            </ListItemAvatar>
-            <ListItemText
-              primary={employee.name}
-              primaryTypographyProps={{ variant: 'subtitle1' }}
-              secondary={`Joined ${dayjs(employee.lastUpdated).format('MMM D, YYYY')}`}
-              secondaryTypographyProps={{ variant: 'body2' }}
-            />
-          </ListItem>
-        ))}
-      </List>
+      {loading ? (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', padding: '120px 0px' }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <List>
+          {employData.map((employee, index) => (
+            <ListItem divider={index < employData.length - 1} key={index}>
+              <ListItemAvatar>
+                {employee.image ? (
+                  <Box
+                    component="img"
+                    src={employee.image}
+                    sx={{ borderRadius: 1, height: '48px', width: '48px' }}
+                  />
+                ) : (
+                  <Box
+                    sx={{
+                      borderRadius: 1,
+                      backgroundColor: 'var(--mui-palette-neutral-200)',
+                      height: '48px',
+                      width: '48px',
+                    }}
+                  />
+                )}
+              </ListItemAvatar>
+              <ListItemText
+                primary={employee.name}
+                primaryTypographyProps={{ variant: 'subtitle1' }}
+                secondary={`Joined ${dayjs(employee.lastUpdated).format('MMM D, YYYY')}`}
+                secondaryTypographyProps={{ variant: 'body2' }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      )}
+
       <Divider />
       <CardActions sx={{ justifyContent: 'flex-end' }}>
         <Link href="/dashboard/employ" passHref>
